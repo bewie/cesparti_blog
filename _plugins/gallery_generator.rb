da05ca6ae22f92723b27aff14f1f9593d3a6d449
laurent <laurent@bewie.org>
2014-05-12 10:07:21 +0200

    Add multidomain support for images gallery.

--- _plugins/gallery_generator.rb
+++ _plugins/gallery_generator.rb
@@ -30,7 +30,11 @@ module Jekyll
       rescue Exception => e
         puts e
       end
-      galleries.each {|gallery| self.data["galleries"].push(gallery.data)}
+      #galleries.each {|gallery| self.data["galleries"].push(gallery.data)}
+      galleries.each do |gallery| 
+        #puts self.data["galleries"]
+        self.data["galleries"].push(gallery.data)
+      end
     end
   end
 
@@ -41,11 +45,16 @@ module Jekyll
       @dest_dir = dir.gsub("source/", "")
       @dir = @dest_dir
       @name = "index.html"
-      @images = []
+      @images = {}
 
       best_image = nil
       max_size_x = 400
       max_size_y = 400
+      post_size_x = 1200
+      post_size_y = 1200
+
+      image_base = site.config["gallery"]["url"] || [""]
+
       scale_method = site.config["gallery"]["scale_method"] || "fit"
       begin
         max_size_x = site.config["gallery"]["thumbnail_size"]["x"]
@@ -62,34 +71,51 @@ module Jekyll
       gallery_name = gallery_name.gsub("_", " ").gsub(/\w+/) {|word| word.capitalize}
       self.data["name"] = gallery_name
       self.data["title"] = "#{gallery_title_prefix}#{gallery_name}"
-      thumbs_dir = "#{site.dest}/#{@dest_dir}/thumbs"
+
+      thumbs = "#{@dest_dir}/thumbs"
+      thumbs_post = "#{@dest_dir}/post_thumbs"
+
+      thumbs_dir = "#{site.dest}/#{thumbs}"
+      thumbs_post_dir = "#{site.dest}/#{thumbs_post}"
 
       FileUtils.mkdir_p(thumbs_dir, :mode => 0755)
+      FileUtils.mkdir_p(thumbs_post_dir, :mode => 0755)
       Dir.foreach(dir) do |image|
         if image.chars.first != "." and image.downcase().end_with?(*$image_extensions)
-          @images.push(image)
+          #@images.push(image)
+          @images[image] = dir
           best_image = image
-          @site.static_files << GalleryFile.new(site, base, "#{@dest_dir}/thumbs/", image)
+          @site.static_files << GalleryFile.new(site, base, "#{thumbs}/", image)
+          @site.static_files << GalleryFile.new(site, base, "#{thumbs_post}/", image)
+
           if File.file?("#{thumbs_dir}/#{image}") == false or File.mtime("#{dir}/#{image}") > File.mtime("#{thumbs_dir}/#{image}")
             begin
               m_image = ImageList.new("#{dir}/#{image}")
               m_image.send("resize_to_#{scale_method}!", max_size_x, max_size_y)
               puts "Writing thumbnail to #{thumbs_dir}/#{image}"
               m_image.write("#{thumbs_dir}/#{image}")
+
+              m_image2 = ImageList.new("#{dir}/#{image}")
+              m_image2.send("resize_to_fit!", post_size_x, post_size_y)
+              puts "Writing thumb for post to #{thumbs_post_dir}/#{image}"
+              m_image2.write("#{thumbs_post_dir}/#{image}")
             rescue
               puts "error"
               puts $!
             end
             GC.start
           end
+
         end
       end
+      self.data["image_base"] = image_base
       self.data["images"] = @images.sort
       begin
         best_image = site.config["gallery"]["galleries"][self.data["gallery"]]["best_image"]
       rescue
       end
       self.data["best_image"] = best_image
+      #puts best_image
       begin
         self.data["date_time"] = EXIFR::JPEG.new("#{dir}/#{best_image}").date_time.to_i
       rescue
